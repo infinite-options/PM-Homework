299d4146a7d54376c2a3b37886f1b31048a37507
pmarathay <pmarathay@infiniteoptions.com>
2022-12-01 09:02:02 -0800

    trying to hide loader - not working

--- Bond-Homework/bondscript.js
+++ Bond-Homework/bondscript.js
@@ -18,34 +18,45 @@ console.log("Setting Info:", settings);
 
 //Target the Element you want to work with
 const mainContent = document.querySelector(".main-content");
-console.log("I'm Here 1");
+const loadButton = document.querySelector("#load");
+const loader = document.querySelector(".loader");
 
-// Declare a function:
-// const functionName = (parameter to pass in) => {actual function code}
-async function displayBondData(input) {
-  // const displayData = (input) => {
-  console.log("If there were an input, it would be:", input);
+loadButton.addEventListener("click", () => {
+  loader.classList.toggle("hidden");
+  loadButton.classList.toggle("hidden");
+  setTimeout(() => {
+    console.log("I'm Here 1");
 
-  // function purpose is to get the data from the API
-  // Set up fetch request inside the fuction.  Fetch has a promise so need a .then
-  // Then, once we fullfil the promise and get data in return do the .then and run a new function as a json object
-  fetch(`https://iznfqs92n3.execute-api.us-west-1.amazonaws.com/dev/api/v2/movies`)
-    .then(function (response) {
-      console.log("API reponse - this is not helpful:", response.json);
-      return response.json();
-    })
+    // Declare a function:
+    // const functionName = (parameter to pass in) => {actual function code}
+    async function displayBondData(input) {
+      // const displayData = (input) => {
+      console.log("If there were an input, it would be:", input);
 
-    // not sure I understand why this function is in .then
-    .then(function (data) {
-      console.log("Data:", data);
-      console.log("Specific Data:", data[0].movie_uid);
-      console.log("Specific Movie:", data[0].director);
-      // bondMovie.innerHTML = data[0].movie_title;
-      // bondActor.innerHTML = data[0].bond_actor;
-      // bondDirector.innerHTML = data[0].director;
+      // function purpose is to get the data from the API
+      // Set up fetch request inside the fuction.  Fetch has a promise so need a .then
+      // Then, once we fullfil the promise and get data in return do the .then and run a new function as a json object
+      fetch(`https://iznfqs92n3.execute-api.us-west-1.amazonaws.com/dev/api/v2/movies`)
+        .then(function (response) {
+          console.log("API reponse - this is not helpful:", response.json);
+          return response.json();
+        })
 
-      mainContent.innerHTML = bondCard(data);
-    });
-}
+        // not sure I understand why this function is in .then
+        .then(function (data) {
+          console.log("Data:", data);
+          console.log("Specific Data:", data[0].movie_uid);
+          console.log("Specific Movie:", data[0].director);
+          // bondMovie.innerHTML = data[0].movie_title;
+          // bondActor.innerHTML = data[0].bond_actor;
+          // bondDirector.innerHTML = data[0].director;
 
-displayBondData();
+          // mainContent.innerHTML = bondCard(data);
+        });
+    }
+
+    displayBondData();
+
+    mainContent.innerHTML = bondCard(data);
+  }, 3000);
+});
