6ffe05c530df759157c614256a057d8c03b9823a
pmarathay <pmarathay@infiniteoptions.com>
2022-12-02 15:38:37 -0800

    commented 3 Working options

--- Bond-Homework/bondscript.js
+++ Bond-Homework/bondscript.js
@@ -17,11 +17,11 @@ console.log("Setting Info:", settings);
 // const bondDirector = document.querySelector(".getDirector");
 
 //Target the Element you want to work with
-const mainContent = document.querySelector(".main-content");
-const loadButton = document.querySelector("#load");
-const loader = document.querySelector(".loader");
+// const mainContent = document.querySelector(".main-content");
+// const loadButton = document.querySelector("#load");
+// const loader = document.querySelector(".loader");
 
-// Working Code without Timeout using bondCard
+// // Option 1: Working Code without Timeout using bondCard
 // loadButton.addEventListener("click", () => {
 //   // loader.classList.toggle("hidden");
 //   // console.log("Load button status:", loadButton.classList);
@@ -38,76 +38,74 @@ const loader = document.querySelector(".loader");
 //     });
 // });
 
-// Working Code without Timeout withou using bondCard
-loadButton.addEventListener("click", () => {
-  // loader.classList.toggle("hidden");
-  // console.log("Load button status:", loadButton.classList);
-  // loadButton.classList.toggle("hidden");
-
-  fetch(`https://iznfqs92n3.execute-api.us-west-1.amazonaws.com/dev/api/v2/movies`)
-    .then(function (response) {
-      console.log("in bondscript.js - After Fetch");
-      return response.json();
-    })
-    .then(function (data) {
-      console.log("in bondscript.js - Data Received:", data);
-      mainContent.innerHTML = `
-        <article class="bondcard">
-        
-          <link href="loader.css" rel="stylesheet" />
-          <span class="loader hidden"> </span>
-
-          <div>
-            <div>Hello Mr. BondCard</div>
-          </div>
-
-          <ul>
-            <!-- NOTE: span is defined in line as getMovie with content N/A -->
-            <li>Bond Movie: <span class="bondcard__movie">${data[0].movie_title}</span> Add other text here</li>
-            <li>Bond Actor: <span class="getBond">N/A</span> Does this work</li>
-            <li>Bond Director: <span class="getDirector">N/A</span> Add other text here</li>
-          </ul>
-        </article>
-    `;
-    });
-});
-
-// Original Code
+// Option 2: Working Code without Timeout without using bondCard
 // loadButton.addEventListener("click", () => {
 //   // loader.classList.toggle("hidden");
+//   // console.log("Load button status:", loadButton.classList);
 //   // loadButton.classList.toggle("hidden");
-//   console.log("I'm Here 1");
 
-//   // Declare a function:
-//   // const functionName = (parameter to pass in) => {actual function code}
-//   async function displayBondData(input) {
-//     // const displayData = (input) => {
-//     console.log("If there were an input, it would be:", input);
+//   fetch(`https://iznfqs92n3.execute-api.us-west-1.amazonaws.com/dev/api/v2/movies`)
+//     .then(function (response) {
+//       console.log("in bondscript.js - After Fetch");
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log("in bondscript.js - Data Received:", data);
+//       mainContent.innerHTML = `
+//         <article class="bondcard">
 
-//     // function purpose is to get the data from the API
-//     // Set up fetch request inside the fuction.  Fetch has a promise so need a .then
-//     // Then, once we fullfil the promise and get data in return do the .then and run a new function as a json object
-//     fetch(`https://iznfqs92n3.execute-api.us-west-1.amazonaws.com/dev/api/v2/movies`)
-//       .then(function (response) {
-//         console.log("API reponse - this is not helpful:", response.json);
-//         return response.json();
-//       })
+//           <link href="loader.css" rel="stylesheet" />
+//           <span class="loader hidden"> </span>
 
-//       // not sure I understand why this function is in .then
-//       .then(function (data) {
-//         console.log("instead of going to bondCard.js", data);
-//         console.log("Data:", data);
-//         console.log("Specific Data:", data[0].movie_uid);
-//         console.log("Specific Movie:", data[0].director);
-//         // bondMovie.innerHTML = data[0].movie_title;
-//         // bondActor.innerHTML = data[0].bond_actor;
-//         // bondDirector.innerHTML = data[0].director;
+//           <div>
+//             <div>Hello Mr. BondCard</div>
+//           </div>
 
-//         // mainContent.innerHTML = bondCard(data);
-//       });
-//   }
-
-//   displayBondData();
-
-//   mainContent.innerHTML = bondCard(data);
+//           <ul>
+//             <!-- NOTE: span is defined in line as getMovie with content N/A -->
+//             <li>Bond Movie: <span class="bondcard__movie">${data[0].movie_title}</span> Add other text here</li>
+//             <li>Bond Actor: <span class="getBond">N/A</span> Does this work</li>
+//             <li>Bond Director: <span class="getDirector">N/A</span> Add other text here</li>
+//           </ul>
+//         </article>
+//     `;
+//     });
 // });
+
+// Option 3: Original Code - Just like Option 1 but with an async function
+loadButton.addEventListener("click", () => {
+  // loader.classList.toggle("hidden");
+  // loadButton.classList.toggle("hidden");
+  console.log("I'm Here 1");
+
+  // Declare a function:
+  // const functionName = (parameter to pass in) => {actual function code}
+  async function displayBondData(input) {
+    // const displayData = (input) => {
+    console.log("If there were an input, it would be:", input);
+
+    // function purpose is to get the data from the API
+    // Set up fetch request inside the fuction.  Fetch has a promise so need a .then
+    // Then, once we fullfil the promise and get data in return do the .then and run a new function as a json object
+    fetch(`https://iznfqs92n3.execute-api.us-west-1.amazonaws.com/dev/api/v2/movies`)
+      .then(function (response) {
+        console.log("API reponse - this is not helpful:", response.json);
+        return response.json();
+      })
+
+      // not sure I understand why this function is in .then
+      .then(function (data) {
+        console.log("instead of going to bondCard.js", data);
+        console.log("Data:", data);
+        console.log("Specific Data:", data[0].movie_uid);
+        console.log("Specific Movie:", data[0].director);
+        // bondMovie.innerHTML = data[0].movie_title;
+        // bondActor.innerHTML = data[0].bond_actor;
+        // bondDirector.innerHTML = data[0].director;
+
+        mainContent.innerHTML = bondCard(data);
+      });
+  }
+
+  displayBondData();
+});
