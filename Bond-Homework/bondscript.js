4d109da0720c523e7187ed07a8dea0d1f7786c6f
pmarathay <pmarathay@infiniteoptions.com>
2022-12-02 15:03:22 -0800

    working bond code with and without bondCard.js

--- Bond-Homework/bondscript.js
+++ Bond-Homework/bondscript.js
@@ -21,73 +21,93 @@ const mainContent = document.querySelector(".main-content");
 const loadButton = document.querySelector("#load");
 const loader = document.querySelector(".loader");
 
+// Working Code without Timeout using bondCard
+// loadButton.addEventListener("click", () => {
+//   // loader.classList.toggle("hidden");
+//   // console.log("Load button status:", loadButton.classList);
+//   // loadButton.classList.toggle("hidden");
+
+//   fetch(`https://iznfqs92n3.execute-api.us-west-1.amazonaws.com/dev/api/v2/movies`)
+//     .then(function (response) {
+//       console.log("in bondscript.js - After Fetch");
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log("in bondscript.js - Data Received:", data);
+//       mainContent.innerHTML = bondCard(data);
+//     });
+// });
+
+// Working Code without Timeout withou using bondCard
 loadButton.addEventListener("click", () => {
   // loader.classList.toggle("hidden");
   // console.log("Load button status:", loadButton.classList);
   // loadButton.classList.toggle("hidden");
 
-  setTimeout(() => {
-    fetch(`https://iznfqs92n3.execute-api.us-west-1.amazonaws.com/dev/api/v2/movies`)
-      .then(function (response) {
-        console.log("in bondscript.js - After Fetch");
-        return response.json();
-      })
-      .then(function (data) {
-        console.log("in bondscript.js - Data Received:", data);
-        mainContent.innerHTML = bondCard(data);
-      });
-  }, 10);
+  fetch(`https://iznfqs92n3.execute-api.us-west-1.amazonaws.com/dev/api/v2/movies`)
+    .then(function (response) {
+      console.log("in bondscript.js - After Fetch");
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("in bondscript.js - Data Received:", data);
+      mainContent.innerHTML = `
+        <article class="bondcard">
+        
+          <link href="loader.css" rel="stylesheet" />
+          <span class="loader hidden"> </span>
 
-  // setTimeout(() => {
-  //   fetch(`https://iznfqs92n3.execute-api.us-west-1.amazonaws.com/dev/api/v2/movies`)
-  //     .then(function (response) {
-  //       console.log("in bondscript.js - After Fetch");
-  //       return response.json();
-  //     })
-  //     .then(function (data) {
-  //       console.log("in bondscript.js - Data Received:", data);
-  //       mainContent.innerHTML = bondCard(data);
-  //     });
-  // }, 10);
+          <div>
+            <div>Hello Mr. BondCard</div>
+          </div>
+
+          <ul>
+            <!-- NOTE: span is defined in line as getMovie with content N/A -->
+            <li>Bond Movie: <span class="bondcard__movie">${data[0].movie_title}</span> Add other text here</li>
+            <li>Bond Actor: <span class="getBond">N/A</span> Does this work</li>
+            <li>Bond Director: <span class="getDirector">N/A</span> Add other text here</li>
+          </ul>
+        </article>
+    `;
+    });
 });
 
 // Original Code
 // loadButton.addEventListener("click", () => {
-//   loader.classList.toggle("hidden");
-//   loadButton.classList.toggle("hidden");
-//   setTimeout(() => {
-//     console.log("I'm Here 1");
+//   // loader.classList.toggle("hidden");
+//   // loadButton.classList.toggle("hidden");
+//   console.log("I'm Here 1");
 
-//     // Declare a function:
-//     // const functionName = (parameter to pass in) => {actual function code}
-//     async function displayBondData(input) {
-//       // const displayData = (input) => {
-//       console.log("If there were an input, it would be:", input);
+//   // Declare a function:
+//   // const functionName = (parameter to pass in) => {actual function code}
+//   async function displayBondData(input) {
+//     // const displayData = (input) => {
+//     console.log("If there were an input, it would be:", input);
 
-//       // function purpose is to get the data from the API
-//       // Set up fetch request inside the fuction.  Fetch has a promise so need a .then
-//       // Then, once we fullfil the promise and get data in return do the .then and run a new function as a json object
-//       fetch(`https://iznfqs92n3.execute-api.us-west-1.amazonaws.com/dev/api/v2/movies`)
-//         .then(function (response) {
-//           console.log("API reponse - this is not helpful:", response.json);
-//           return response.json();
-//         })
+//     // function purpose is to get the data from the API
+//     // Set up fetch request inside the fuction.  Fetch has a promise so need a .then
+//     // Then, once we fullfil the promise and get data in return do the .then and run a new function as a json object
+//     fetch(`https://iznfqs92n3.execute-api.us-west-1.amazonaws.com/dev/api/v2/movies`)
+//       .then(function (response) {
+//         console.log("API reponse - this is not helpful:", response.json);
+//         return response.json();
+//       })
 
-//         // not sure I understand why this function is in .then
-//         .then(function (data) {
-//           console.log("Data:", data);
-//           console.log("Specific Data:", data[0].movie_uid);
-//           console.log("Specific Movie:", data[0].director);
-//           // bondMovie.innerHTML = data[0].movie_title;
-//           // bondActor.innerHTML = data[0].bond_actor;
-//           // bondDirector.innerHTML = data[0].director;
+//       // not sure I understand why this function is in .then
+//       .then(function (data) {
+//         console.log("instead of going to bondCard.js", data);
+//         console.log("Data:", data);
+//         console.log("Specific Data:", data[0].movie_uid);
+//         console.log("Specific Movie:", data[0].director);
+//         // bondMovie.innerHTML = data[0].movie_title;
+//         // bondActor.innerHTML = data[0].bond_actor;
+//         // bondDirector.innerHTML = data[0].director;
 
-//           // mainContent.innerHTML = bondCard(data);
-//         });
-//     }
+//         // mainContent.innerHTML = bondCard(data);
+//       });
+//   }
 
-//     displayBondData();
+//   displayBondData();
 
-//     mainContent.innerHTML = bondCard(data);
-//   }, 3000);
+//   mainContent.innerHTML = bondCard(data);
 // });
